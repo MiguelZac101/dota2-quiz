2e8e8a43c227a7b78fdb7e2cae0d3a4695530f39
zack <miguel.quispe@fractal.com.pe>
2026-05-12 21:19:01 -0500

    feat: extend Hero type with additional attributes

--- src/types/hero.ts
+++ src/types/hero.ts
@@ -3,4 +3,10 @@ export type Hero = {
     name: string
     localized_name: string
     img: string
+    primary_attr: 'str' | 'agi' | 'int' | 'all'
+    attack_type: 'Melee' | 'Ranged'
+    roles: string[]
+    move_speed: number
+    base_armor: number
+    legs: number
 }
